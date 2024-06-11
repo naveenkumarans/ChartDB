b8c75d20c78839824dacd78e3af55170c9b94a1f
naveen59835 <naveenkumar59835@gmail.com>
2024-06-11 22:51:58 +0530

    added cors

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Naveen Kumar
+ * Date : 11-06-2024
+ * Created With : IntelliJ IDEA Community Edition
+ */
+
+package com.example.demo.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
